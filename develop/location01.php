704ac5c440801049ea6eb1f580c96db139c48f56
橋本真奈美 <manako.075h@gmail.com>
2023-08-03 16:41:38 +0900

    no message

--- develop/location01.php
+++ develop/location01.php
@@ -5,13 +5,13 @@
   "gnav_set" => "location",
   "page_class" => "location01",
   "url" => "location01.html",
-  "title" => "上質な住環境が広がる桜上水｜ロケーション｜【公式】シティテラス桜上水｜桜上水の新築マンション｜住友不動産",
-  "keywords" => "ロケーション,シティテラス桜上水,桜上水 新築マンション,桜上水,桜上水駅,京王電鉄京王線,世田谷区上北沢,マンション,新築マンション,分譲マンション,住友不動産",
-  "description" => "ロケーションページ。京王電鉄京王線「桜上水駅」から徒歩12分。住友不動産の新築マンションが桜上水に誕生。【すみふ桜上水】世田谷区上北沢の新築マンションなら住友不動産。",
+  "title" => "土地区画整理事業で生まれ変わる街｜ロケーション｜【【公式】シティテラス登戸｜登戸の新築マンション｜住友不動産",
+  "keywords" => "ロケーション,シティテラス登戸,登戸 新築マンション,登戸,登戸駅,京王電鉄京王線,世田谷区上北沢,マンション,新築マンション,分譲マンション,住友不動産",
+  "description" => "ロケーションページ。京王電鉄京王線「登戸駅」から徒歩11分。住友不動産の新築マンションが登戸に誕生。【すみふ登戸】世田谷区上北沢の新築マンションなら住友不動産。",
   "add_stylesheet" => ["css/location.css","css/location01.css"],
   "add_script" =>  [],
     
-  "logo_text" => "桜上水駅徒歩12分｜シティテラス桜上水｜桜上水 新築マンション｜ロケーション｜すみふ桜上水｜住友不動産"
+  "logo_text" => "登戸駅徒歩11分｜シティテラス登戸｜登戸 新築マンション｜ロケーション｜すみふ登戸｜住友不動産"
 );
 ?>
 
@@ -21,172 +21,145 @@
 	<!-- パンくずリスト -->
   <div class="breadcrumb sp-hide">
     <ul>
-      <li><a href="./index.html">シティテラス桜上水｜桜上水 新築マンション｜トップ</a></li>
+      <li><a href="./index.html">シティテラス登戸｜登戸 新築マンション｜トップ</a></li>
       <li><a href="./location01.html">ロケーション</a></li>
-      <li><p>上質な住環境が広がる桜上水</p></li>
+      <li><p>土地区画整理事業で生まれ変わる街</p></li>
     </ul>
   </div>
 	<!--// パンくずリスト -->
 
-  <div class="location-link location-link--2">
-    <a href="location01.html" class="current"><p>上質な住環境が広がる桜上水</p></a>
-    <a href="location02.html"><p>家族の憩いの場が広がる桜上水</p></a>
+  <div class="location-link location-link--3">
+    <a href="location01.html" class="current"><p>土地区画整理事業で生まれ変わる街</p></a>
+    <a href="location02.html"><p>水と緑の自然を身近に</p></a>
+    <a href="location03.html"><p>「登戸」駅周辺の優れた利便性</p></a>
   </div>
 
   <header class="main-visual">
-    <h2 class="page-lead gothic">Hello Beautiful Future.</h2>
-   <div class="image"><img src="./images/location01/mv01.jpg" alt="" class="pc"><img src="./images/location01/mv01-sp.jpg" alt="" class="sp"><p class="annotation white">航空写真</p></div>
+    <!--<h2 class="page-lead gothic">Hello Beautiful Future.</h2>-->
+    <div class="image"><img src="./images/location01/mv01.jpg" alt="" class="pc"><img src="./images/location01/mv01-sp.jpg" alt="" class="sp"><p class="annotation white">image photo</p></div>
   </header><!-- main-visual -->
 
   <section class="sections intro">
     <div class="inner">
-      <h2 class="section-title gothic">LOCATION</h2>
-      <p class="lead">次なる桜上水へ。</p>
-      <p class="text">快適な都心アクセス<sup>※1</sup>をかなえる<br>京王線急行停車駅「桜上水」。<br>その利便性は未来に向けてさらなる進化を果たします。<br>連続立体交差事業が現在進行中。<br>次代へと生まれ変わる「桜上水」のポテンシャルが<br>ますます高まっていきます。</p>
+      <h2 class="section-title">LOCATION</h2>
+    </div>
+    <div class="wrap">
+      <p class="lead">土地区画整理事業で生まれ変わる街。</p>
     </div>
   </section><!-- intro -->
 
-  <section class="sections valuable">
-    <div class="section-img image"><img src="./images/location01/img01.jpg" alt=""><p class="annotation">日本大学文理学部正門前（徒歩11分／約820m）</p></div>
-    <div class="inner">
-      <h2 class="section-title gothic">VALUABLE</h2>
-      <p class="lead">平穏と安心の住環境へ。</p>
-    </div>
-    <div class="wrap cond01">
-      <p class="lead02">厳しい建築制限によって住環境が守られる<br>第一種低層住居専用地域。</p>
-      <p class="text">シティテラス桜上水の建設地とその周囲は、第一種低層住居専用地域に指定されたエリアです。第一種低層住居専用地域とは、13種ある用途地域でもっとも厳しい建築制限が定められ、大規模な店舗や事務所、大学や病院、ホテル、工場だけでなく、パチンコ店やカラオケ店、風俗施設など住環境を悪化させる懸念のある施設の建設が厳しく制限されています。また、建物の高さ、建ぺい率、容積率に制限があるため、良好な住環境が保護された地域と言えるでしょう。</p>
-      <ul class="imgs-lists imgs-list01">
-        <li class="cond01">
-          <div class="img"><img src="./images/location01/img02.jpg" alt="" class="sp-hide"><img src="./images/location01/img02.jpg" alt="" class="zoomImg sp"></div>
-          <p class="annotation">用途地域概念図　出典：世田谷区・杉並区ホームページ</p>
-        </li>
-        <li class="cond02">
-          <div class="img"><img src="./images/location01/img03.svg" alt="用途凡例"></div>
-        </li>
-      </ul>
-    </div><!-- wrap cond01 -->
-    <div class="wrap cond02">
-      <p class="lead02">堅固な地盤で液状化の不安が少ない<br>安心の武蔵野台地。</p>
-      <p class="text">約２万年もの歳月をかけて堆積した関東ローム層により形成された強固な地盤、武蔵野台地。シティテラス桜上水の建設地は武蔵野台地の標高約43.7mに位置しています。湾岸エリアや都東部の低地に比べて、地震や自然災害による液状化の不安が少ない地盤と言えます。</p>
-      <ul class="imgs-lists imgs-list02">
-        <li class="cond01">
-          <div class="img"><img src="./images/location01/img04.jpg" alt="" class="sp-hide"><img src="./images/location01/img04.jpg" alt="" class="zoomImg sp"></div>
-          <p class="annotation">武蔵野台地概念イラスト</p>
-        </li>
-        <li class="cond02">
-          <div class="img"><img src="./images/location01/img05.svg" alt="標高比率"></div>
-        </li>
-      </ul>
-    </div><!-- wrap cond02 -->
-  </section><!-- valuable -->
 
-
-  <section class="sections reborn">
-    <div class="section-img image"><img src="./images/location01/img06.jpg" alt=""><p class="annotation">提供：京王電鉄</p></div>
-    <div class="inner">
-      <h2 class="section-title gothic">REBORN</h2>
-      <p class="lead">真価を求める進化へ。</p>
+  <div class="sections history clearfix">
+    <div class="img img01">
+      <img src="./images/location01/img01.jpg" alt="">
+      <p class="annotation white">image photo</p>
     </div>
     <div class="wrap">
-      <p class="lead02">駅前をもっと安全、快適、そして便利に。<br>京王電鉄京王線（「笹塚」駅&#x301c;「仙川」駅）間連続立体交差事業<br><span class="mini">（2012年都市計画変更決定、2013年度事業着手）</span></p>
-      <p class="text">京王線の「笹塚」駅から「仙川」駅間付近にかけて進められている道路と鉄道との連続立体交差事業。これによって、道路と鉄道それぞれの安全性が向上。また、高架化によってバスなど交通機関相互の乗り継ぎがしやすくなるよう、世田谷区「京王線沿線街づくり基本方針」に基づく「京王線沿線駅前広場基本構想」が2009年に取りまとめられ、具体化に向け検討されています。</p>
-      <div class="img image01"><img src="./images/location01/img07.png" alt="" class="sp-hide"><img src="./images/location01/img07.png" alt="" class="zoomImg sp"><p class="annotation">京王線路線概念図</p></div>
-      
-
-      <div class="stations station01">
-        <div class="imgs">
-          <div class="photo"><img src="./images/location01/img09.jpg" alt=""></div>
-          <p class="annotation"> 提供：京王電鉄 ※現時点（2023年3月）でのイメージであり実際とは異なる場合があります。</p>
-        </div>
-        <div class="text-box">
-          <div class="texts">
-            <p class="txt01">京王線「桜上水」駅</p>
-            <p class="txt02">木質調等、自然と調和する質感や柔らかな配色により、穏やかな住宅街と共存するデザインに。</p>
-          </div>
-          <div class="img"><img src="./images/location01/img08.png" alt="「桜上水」駅高架化概念図"></div>
-        </div><!-- text-box -->
-      </div><!-- station01 -->
-
-      <div class="stations station02">
-        <div class="imgs">
-          <div class="photo"><img src="./images/location01/img10.jpg" alt=""></div>
-          <p class="annotation">提供：京王電鉄 ※現時点（2023年3月）でのイメージであり実際とは異なる場合があります。</p>
-        </div>
-        <div class="text-box">
-          <div class="texts">
-            <p class="txt01">京王線「上北沢」駅</p>
-            <p class="txt02">落ち着いた色合いと透明感のある素材を組み合わせることにより、地元のシンボルである桜並木が映えるデザインに。</p>
-          </div>
-        </div><!-- text-box -->
-      </div><!-- station02 -->
-
-      <div class="lists-wrap">
-        <ul class="lists clearfix">
-          <li>
-            <div class="img"><img src="./images/location01/img11.jpg" alt=""></div>
-            <p class="name">京王線「代田橋」駅</p>
-          </li>
-          <li>
-            <div class="img"><img src="./images/location01/img12.jpg" alt=""></div>
-            <p class="name">京王線「明大前」駅</p>
-          </li>
-          <li>
-            <div class="img"><img src="./images/location01/img13.jpg" alt=""></div>
-            <p class="name">京王線「下高井戸」駅</p>
-          </li>
-        </ul>
-        <ul class="lists list02 clearfix">
-          <li>
-            <div class="img"><img src="./images/location01/img14.jpg" alt=""></div>
-            <p class="name">京王線「芦花公園」駅</p>
-          </li>
-          <li>
-            <div class="img"><img src="./images/location01/img15.jpg" alt=""></div>
-            <p class="name">京王線「千歳烏山」駅</p>
-          </li>
-        </ul>
-        <p class="list-annotation">提供：京王電鉄 ※現時点（2023年3月）でのイメージであり実際とは異なる場合があります。</p>
-      </div><!-- lists-wrap -->
-
-    </div><!-- wrap -->
-  </section><!-- reborn -->
-
-
-  <section class="sections planning">
-    <div class="section-img image"><img src="./images/location01/img16.jpg" alt=""><p class="annotation">「桜上水」駅（徒歩12分／約920m）</p></div>
-    <div class="inner">
-      <h2 class="section-title gothic">TOWN PLANNING</h2>
-      <p class="lead">調和と交流のまちづくり。</p>
+    <p class="box-tit">HISTORY</p>
+    <p class="text">江戸時代から交通の要衝として賑わった「登戸宿」。</p>
+    <div class="img img02">
+      <img src="./images/location01/img02.jpg" alt="">
     </div>
+    <p class="mini">「江戸名所図会巻之三」1834年長谷川雪旦画の一部を加工・国立国会図書館蔵</p>
+    <div class="texts">
+      <p class="tit">津久井道の宿場町だった「登戸宿」。</p>
+      <p class="lead">江戸時代、江戸と相模（現在の世田谷区方面と相模原市津久井方面）を結ぶ「津久井道」には、周辺の名産である禅寺柿や黒川炭、津久井の絹、また多摩川上流から流される木材や海路からの石材などが運ばれ、多くの人々が往来する街道でした。そんな「津久井道」の宿場町として発展した「登戸宿」。登戸はこのころから交通の要衝として賑わっていました。</p>
+    </div>
+    <ul class="texts">
+      <li>
+        <div class="img img03">
+          <img src="./images/location01/img03.jpg" alt="">
+        </div>
+      </li>
+      <li>
+        <p class="tit">小田急線開通、「稲田登戸」駅開業。</p>
+        <p class="lead">1927年、新宿～小田原間の小田急線開通と同時に、小田急線の五大停車場のひとつとして「稲田登戸」駅（現「向ヶ丘遊園」駅）が開業しました。またJR南武線は私鉄の南武鉄道として、小田急線と同じ年の1927年3月に「川崎」駅から「登戸」駅間が開通し、2年後の1929年には「立川」駅まで全線開通。1936年には小田急線との間に登戸連絡線が作られ、貨車などの相互乗り入れが始まりました。</p>
+      </li>
+    </ul>
+    </div><!-- wrap -->
+  </div><!-- history -->
+
+
+
+  <div class="sections redevelopment clearfix">
     <div class="wrap">
-      <p class="lead02">生活拠点としての賑わいを創出する<br>桜上水駅周辺地区地区街づくり計画。</p>
-      <p class="text">世田谷区桜上水4丁目と5丁目で推進されている街づくり計画は、以下の目標を掲げ進められています。</p>
-      <ol class="points">
-        <li>地区生活拠点として桜上水駅周辺の賑わいが広がる街づくり</li>
-        <li>緑が溢れ心安らぐ住環境の維持、保全と向上を進める街づくり</li>
-        <li>災害に強く、誰にでもやさしい、安全で安心な街づくり</li>
-        <li>人とまちが調和し、様々な世代が交流できる街づくり</li>
-      </ol>
-      <p class="points-annotation">出典：世田谷区ホームページ</p>
-      <ul class="imgs-lists">
-        <li class="cond01">
-          <div class="img"><img src="./images/location01/img17.jpg" alt="住宅地区　地図" class="sp-hide"><img src="./images/location01/img17.jpg" alt="住宅地区　地図" class="zoomImg sp"></div>
-          <p class="annotation">出典：世田谷区ホームページ</p>
+      <p class="box-tit">REDEVELOPMENT</p>
+      <p class="tit">登戸土地区画整理事業<sup>※</sup></p>
+      <ul class="list01">
+        <li>
+          <p class="text">2026年完了予定の土地区画整理事業が進行中。<br>快適な街へと進化を続ける「登戸」。</p>
+          <p class="lead">「登戸」駅南側から「向ヶ丘遊園」駅北側の約37.2haのエリアで進む土地区画整理事業。この事業は、幹線道路や区画道路、駅前広場といった主要な基盤施設の整備や3つの公園の新設などを中心とした総合的な土地利用と、川崎市北部の中心的な商業地区として商業やオフィス機能の強化を図るとともに、安全で快適な市街地の形成を目的とした事業です。現在、2026年の完了をめざして、整備が進められています。</p>
+          <p class="mini">※登戸土地区画整理事業（2026年3月完了予定）<br>出典：川崎市ホームページ（2023年6月現在）</p>
         </li>
-        <li class="cond02">
-          <div class="img"><img src="./images/location01/img18.svg" alt="住宅地区　凡例"></div>
+        <li>
+          <div class="img img04">
+            <img src="./images/location01/img04.jpg" alt="">
+          </div>
+          <p class="caps">登戸3号街区公園（約1,130m／徒歩15分）</p>
         </li>
       </ul>
+      <div class="img img05">
+        <img src="./images/location01/img05.png" alt="">
+      </div>
+      <div class="policy">
+        <p class="txt01"><span>街づくりの方針</span></p>
+        <ul class="points">
+          <li class="point01">
+            <p class="txt02"><span>1</span>登戸駅前地区</p>
+            <p class="txt03">都市機能が集積する中心商業地として多くの人が交流し、賑わう街づくり。</p>
+          </li>
+          <li class="point02">
+            <p class="txt02"><span>2</span>向ヶ丘遊園駅前地区</p>
+            <p class="txt03">都市機能が集積する中心商業地として多くの人が交流し、賑わう街づくり。</p>
+          </li>
+          <li class="point03">
+            <p class="txt02"><span>3</span>登戸駅・向ヶ丘遊園駅連携地区</p>
+            <p class="txt03">2駅周辺の中心商業地の機能を補完する多様な業種が立地できる街づくり。</p>
+          </li>
+          <li class="point04">
+            <p class="txt02"><span>4</span>界隈商業地区</p>
+            <p class="txt03">多摩区の文化・業務の拠点に相応しい、文化を感じる緑あふれる街づくり。</p>
+          </li>
+          <li class="point05">
+            <p class="txt02"><span>5</span>界隈共存地区</p>
+            <p class="txt03">多摩区の文化・業務の拠点に相応しい、文化を感じる緑あふれる街づくり。</p>
+          </li>
+          <li class="point06">
+            <p class="txt02"><span>6</span>住宅地地区</p>
+            <p class="txt03">住宅中心の調和のとれた街並みと公園などの緑のある落ち着いた街づくり。</p>
+          </li>
+        </ul>
+        </div><!-- policy -->
+        <p class="tit">登戸2号線沿道まちづくり<sup>※</sup></p>
+        <p class="text">「登戸」と「向ヶ丘遊園」。 <br>2つの駅をつなぎ、人々が回遊する通りをめざして。</p>
+        <p class="lead">「登戸」駅と「向ヶ丘遊園」駅をつなぎ、土地区画整理事業の中心となる「登戸2号線」。この通りではいま「多彩な人々を引き寄せ、人々が楽しみ、憩う通り」をコンセプトに、誰もが安心して歩くことができる幅広い車道・歩道の整備や、沿道にベンチなどを設置した交流の場にも活用できる空間づくりが進められています。</p>
+        <p class="mini">※登戸土地区画整理事業、登戸2号線沿線まちづくり（2026年3月完了予定） 出典：川崎市ホームページ（2023年6月現在）</p>
+        <ul class="list02">
+          <li>
+            <div class="image">
+              <img src="./images/location01/img06.jpg" alt="">
+            </div>
+            <p class="caps">出典：川崎市ホームページ（2023年6月現在）</p>
+          </li>
+          <li>
+            <div class="image">
+              <img src="./images/location01/img07.jpg" alt="">
+            </div>
+            <p class="caps">出典：川崎市ホームページ（2023年6月現在）</p>
+          </li>
+        </ul>
     </div><!-- wrap -->
-  </section><!-- planning -->
+  </div><!-- redevelopment -->
 
 
-  <p class="page-annotation js-scroll wrap">※1.「桜上水」駅より京王線急行利用<br>※京王電鉄京王線（「笹塚」駅&#x301c;「仙川」駅間）連続立体交差事業（2012年都市計画変更決定、2013年度事業着手／出典：京王電鉄株式会社ホームページより2022年11月現在）<br>※掲載の航空写真は世田谷区船橋５丁目付近上空から北方面を撮影（2022年10月）したものに現地部分の光などCG合成・加工をしたものです。また、周辺環境は将来変わる場合があります。<br>※掲載の用途地域概念図は一部道路・施設等を抜粋して表記しています。※掲載の用途地域概念図は、世田谷区の用途地域図を基に引用したものです。※用途地域・周辺環境は将来変わる場合があります。<br>※距離表示については地図上の概測距離を、徒歩分数表示については80ｍを1分として算出し、端数を切り上げたものです。<br>※掲載の武蔵野台地概念イラストは現地を基準とした直線ライン上の一部施設を抜粋した概念図であり、眺望を保証するものではありません。また、建物位置・高さ・距離・縮尺等は実際とは異なり、周辺環境・眺望は将来変わる場合があります。<br>※掲載の標高は「国土地理院ウェブサイト」にて検索（2022年11月現在）したものです。（計測地/現地（東京都世田谷区上北沢1-5）、JR山手線「新宿」駅南口（新宿区新宿3-38-1）、東急東横・目黒線「田園調布」駅（大田区田園調布3-25-18）、東急田園都市・大井町線「二子玉川」駅（世田谷区玉川2-22-13）東京メトロ銀座線「銀座」駅（東京都中央区銀座四丁目1-2）、東京臨海新交通臨海線「台場」駅（東京都港区台場二丁目6先）<br>※用途地域・周辺環境は将来変わる場合があります。</p>
+
+  <p class="page-annotation js-scroll wrap">※1.登戸土地区画整理事業（2026年3月完了予定） 出典：川崎市ホームページ（2023年6月現在）<br>※距離表示については地図上の概測距離を、徒歩分数表示については80ｍを1分として算出し、端数を切り上げたものです。<br>※掲載の登戸土地区画整理事業概念図は一部道路・施設等を抜粋して表記しています。<br>※掲載の写真は2023年6月撮影。<br>※掲載の情報は2023年4月現在のものです。</p>
 </main>
 
-<div class="location-link location-link--2">
-  <a href="location01.html" class="current"><p>上質な住環境が広がる桜上水</p></a>
-  <a href="location02.html"><p>家族の憩いの場が広がる桜上水</p></a>
+<div class="location-link location-link--3">
+  <a href="location01.html" class="current"><p>土地区画整理事業で生まれ変わる街</p></a>
+  <a href="location02.html"><p>土地区画整理事業で生まれ変わる街</p></a>
+  <a href="location03.html"><p>「登戸」駅周辺の優れた利便性</p></a>
 </div>
 
 <?php require(INCLUDES."footer.php"); ?>
