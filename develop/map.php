75e415aad8964165ae27ac62673a911263c973e3
福井彩佳 <ayaka.fsp@gmail.com>
2023-09-26 12:00:55 +0900

    no message

--- develop/map.php
+++ develop/map.php
@@ -18,26 +18,27 @@
 <?php require(INCLUDES."header.php"); ?>
 
 <main>
-	<div class="content-inner">
-    <div class="print-area">
-      <h2 class="page-title js-scroll"><img src="images/map/ttl.jpg" alt="現地案内図"></h2>
-
-      <div class="map-image">
+<section id="submaincontainer" class="map-sec">
+    <h2 class="ttl"><span class="eng">MAP</span><span class="ja">現地案内図</span></h2>
+      <div class="mapArea">
         <div class="image pc"><img src="./images/map/map.jpg" alt="現地案内図" id="map1"></div>
         <figure class="img img05 sp">
           <img src="./images/map/map.jpg" class="sp-hide" alt="">
           <img src="./images/map/map.jpg" class="zoomImg sp" alt=""/>
         </figure>
-      </div><!-- map-image -->
-      <p class="text01 js-scroll">神奈川県川崎市多摩区登戸3816番3（地番）<span>※住戸表示ではありません。</span></p>
-    </div> 
+      </div><!-- mapArea -->
+      <p class="zip js-scroll">神奈川県川崎市多摩区登戸3816番3（地番）<span>※住戸表示ではありません。</span></p>
 
-    <ul class="link-buttons js-scroll">
-      <li class="link"><a href="https://goo.gl/maps/H5tvKTZVVkfrqE4L6" target="_blank">Google Mapsで見る</a></li>
-      <li class="link02 pc"><a href="JavaScript:;" onclick="print_out(); return false;">印刷する</a></li>
-    </ul>
-    <p class="annotation js-scroll">※掲載の現地案内図は一部道路・施設等を抜粋して表記しています。</p>
-  </div>
+    <div id="btnArea">
+			<div id="GoogleMap"><a href="https://goo.gl/maps/H5tvKTZVVkfrqE4L6" target="_blank"><img src="images/map/btn_googlemap.jpg" alt="Google Map"></a></div>
+			<div id="PrintMap"><a href="JavaScript:;" onclick="print_out(); return false;"><img src="images/map/btn_print.jpg" alt="印刷" class="pc"></a></div>
+    </div>
+
+    <div class="container">
+	     <p class="txt">※掲載の現地案内図は一部道路・施設等を抜粋して表記しています。</p>
+    </div>  
+  <!-- </div> -->
+</section>
 </main>
 
 <?php require(INCLUDES."footer.php"); ?>
