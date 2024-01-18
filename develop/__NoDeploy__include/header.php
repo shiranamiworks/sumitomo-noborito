a2ccd6bd6eb48d733ee943d6c1a82afec14836cd
福井彩佳 <ayaka.fsp@gmail.com>
2024-01-18 16:45:15 +0900

    no message

--- develop/__NoDeploy__include/header.php
+++ develop/__NoDeploy__include/header.php
@@ -145,7 +145,7 @@ $folder_name = $site_setting["area"]."/".$site_setting["prop"]."/";
             <li><a href="map.html" target="_blank">現地案内図</a></li>
             <li><a href="/<?= $folder_name?>faq.cgi" target="_blank">FAQ</a></li>
             <li><a href="javascript:outline();">物件概要</a></li>
-            <li><p>マンション情報ブログ</p></li>
+            <li><a href="javascript:blog();">マンション情報ブログ</a></li>
           </ul>
           <div class="header-sns">
             <a href="<?=$site_setting["twitter_link"] ?>" target="_blank"><img src="./images/common/icn_twitter.png" alt="Twitter"></a>
@@ -171,7 +171,7 @@ $folder_name = $site_setting["area"]."/".$site_setting["prop"]."/";
         <li><a href="<?= $page_settings["directory_pos"]  ?>access.html">アクセス</a></li>
         <li><a href="<?= $page_settings["directory_pos"]  ?>map.html" target="_blank">現地案内図</a></li>
         <li><a href="javascript:outline();">物件概要</a></li>
-        <li>マンション情報ブログ</li>
+        <li><a href="javascript:blog();">マンション情報ブログ</a></li>
         <li><a href="/<?= $folder_name?>faq.cgi" target="_blank">FAQ</a></li>
         <script type="text/javascript" src="/bukken_manage/bukken_manage_entry_button.cgi?AREA_CD=shuto&PROP_CD=ct_noborito&LINK=sp-menu"></script>
       </ul>
--- develop/sitemap.php
+++ develop/sitemap.php
@@ -42,8 +42,7 @@
 	<li class="access"><a href="access.html">アクセス</a></li>
 	<li class="map"><a href="map.html" target="_blank">現地案内図</a></li>
 	<li class="outline"><a href="javascript:outline()">物件概要</a></li>
-	<!--li class="blog"><a href="javascript:blog();">マンション情報ブログ</a></li-->
-	<li class="blog">マンション情報ブログ</li>
+	<li class="blog"><a href="javascript:blog();">マンション情報ブログ</a></li>
 	<li class="faq"><a href="/shuto/ct_noborito/faq.cgi" target="_blank">FAQ</a></li>
       <li class="link sitemap"><a href="sitemap.html"><span class="ja">サイトマップ</span></a></li>
       <script type="text/javascript" src="/bukken_manage/bukken_manage_entry_button.cgi?AREA_CD=shuto&PROP_CD=ct_noborito&LINK=footer"></script>
