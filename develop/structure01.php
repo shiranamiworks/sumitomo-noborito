4981a22943665f1590df99faf03ae880327a22b2
橋本真奈美 <manako.075h@gmail.com>
2024-02-28 17:49:46 +0900

    no message

---
+++ develop/structure01.php
@@ -0,0 +1,217 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "structure",
+  "gnav_set" => "structure",
+  "page_class" => "structure01",
+  "url" => "structure01.html",
+  "title" => "基本構造｜",
+  "keywords" => "基本構造,",
+  "description" => "基本構造ページ。",
+  "add_stylesheet" => ["css/structure.css","css/structure01.css"],
+  "add_script" =>  [],
+    
+  "logo_text" => "基本構造｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス登戸｜登戸 マンション｜トップ</a></li>
+      <li><a href="./structure01.html">構造・防災</a></li>
+      <li><p>基本構造</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="structure-link structure-link--3">
+    <a href="structure01.html" class="current"><p>基本構造</p></a>
+    <a href="structure02.html"><p>防災対策</p></a>
+    <a href="structure03.html"><p>セキュリティ</p></a>
+  </div>
+
+  <section class="sections sec01 clearfix">
+    <div class="wrap">
+      <h2 class="tit">基本構造</h2>
+      <div class="tit-img">
+        <img src="./images/structure01/tit01.png" alt="" class="">
+      </div>
+      <ul class="lists list01">
+        <li>
+          <p class="lead">「住宅の品質確保の促進等に関する法律（品確法）」に基づいた、「住宅性能表示制度」による性能評価を受けています。本制度は従来わかりづらかった住まいの性能について、国土交通大臣登録の住宅性能評価機関が同じ基準で、等級（数値）をつけるというものです。</p>
+          <p class="text gothic">● 住宅性能評価書の種類</p>
+          <p class="lead">評価書には、設計図書の段階で評価した結果をまとめた「設計住宅性能評価書」と、施工段階と完成段階の現場での検査を経て評価した結果を記載した「建設住宅性能評価書」の2種類があります。</p>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/structure01/img01.png" alt="" class="">
+          </div>
+        </li>
+      </ul>
+      <p class="text gothic">● 住宅性能表示は項目毎に等級や数値等で表示</p>
+      <p class="lead">品確法による住宅性能表示制度では項目ごとに等級、数値および設計内容等が表示されます。等級が高いほど、性能が高いということです。ただし、これらの性能のなかには「耐震」の項目で高い等級がついても、梁や柱、壁を強くしているために、窓などが小さくなり、開口率などを表わす「光・視環境」の項目では高い数値にならない、という関係もあるため、あくまでも、住まい選びの「目安」として、ご活用ください。</p>
+      <p class="text gothic">住宅性能評価項目</p>
+      <ul class="lists list02">
+        <li>
+          <div class="image">
+            <img src="./images/structure01/img02.png" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <p class="caps">※「音環境に関すること（選択項目）」、「空気環境に関すること」のなかの「室内空気中の化学物質の濃度等（選択項目）」については、評価を取得しておりません。あらかじめご了承ください。<br>※詳細は係員にお尋ねください。</p>
+        </li>
+      </ul>
+      <ul class="lists list03">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit02.png" alt="" class="">
+          </div>
+          <p class="lead">本物件は、外皮の断熱性能などの向上と共に、高効率な設備システムを導入することで、室内環境の質を保ちつつ省エネルギーを実現。</p>
+          <ul class="list04">
+            <li>
+              <p class="lead">共用部を含む住棟全体で年間の一次エネルギー消費量を現行の省エネ基準値から20％以上削減することを目指した基準「ZEH-M Oriented」を取得予定です。</p>
+            </li>
+            <li>
+              <div class="image">
+                <img src="./images/structure01/img03.png" alt="" class="">
+              </div>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit03.png" alt="" class="">
+          </div>
+          <p class="lead">建物の床下を鉄筋コンクリートで固め、地表近くの地盤で建物を面で支える「直接基礎」を採用しています。地下深くに杭を打ち込み、建物を点で支える杭基礎とは異なり、建物直下で面的に建物を支える、安定した基礎構造です。<br><span>※C棟北側出入口など一部除く。</span></p>
+          <div class="image">
+            <img src="./images/structure01/img04.png" alt="" class="">
+          </div>
+        </li>
+      </ul>
+
+      <div class="tit-img">
+        <img src="./images/structure01/tit04.png" alt="" class="">
+      </div>
+      <ul class="lists list05">
+        <li>
+          <p class="lead">本物件は住宅性能表示制度における劣化対策等級3［最高ランク］を取得しています。なお、住戸のある建物は構造躯体の耐久性を高めるために、柱・梁・床等の主要な構造部について、設計基準強度（Fc）を約30N/m㎡に設定しています。 </p>
+          <p class="caps">［劣化対策等級3］</p><p class="caps">劣化対策等級3は、構造躯体等について「通常想定される自然条件および維持管理の条件の下で、3世代（概ね75～90年）まで、大規模な改修を必要とするまでの期間を伸長するために必要な対策が講じられている」ことを認定基準としています。</p><p class="caps">※コンクリートの耐久性は強度が高いほど高くなると言われております。 ※コンクリートの性質上、乾燥収縮や温度変化による収縮に伴うひび割れが発生する場合があります。（一般的に構造上の問題はありません。） ※健全な状態を保つためには、予定されている大規模修繕工事など定期的かつ適切な維持管理が必要となります。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/img05.png" alt="" class="">
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list06">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit05.png" alt="" class="">
+          </div>
+          <p class="lead">住戸のある建物はコンクリートの耐久性を高めるため、柱・梁・床などの主要な構造部について、セメントの重量に対する水の重量の割合を50％以下に設定しています。水セメント比が小さくなるほど、耐久性が大きくなる傾向があると言われています。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit06.png" alt="" class="">
+          </div>
+          <p class="lead">住戸のある建物の耐震壁の鉄筋は、コンクリートの中に二重に鉄筋を配したダブル配筋を採用しています。シングル配筋に比べより高い耐震性を確保します。</p>
+          <div class="image img06">
+            <img src="./images/structure01/img06.png" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit07.png" alt="" class="">
+          </div>
+          <p class="lead">住戸のある建物の主要な柱部分には帯筋の接続部を溶接した、溶接閉鎖型帯筋を採用しました。工場溶接による安定した強度の確保によって、地震時の主筋のはらみ出しを抑制して、コンクリートの拘束力を高めます。<br><span>※柱と梁の接合部を除く。</span></p>
+          <div class="image">
+            <img src="./images/structure01/img07.png" alt="" class="">
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section><!-- sec01 -->
+
+
+
+  <section class="sections sec02 clearfix">
+    <div class="wrap">
+      <h2 class="tit">プライバシー</h2>
+      <ul class="lists list01 clearfix">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit08.png" alt="" class="">
+          </div>
+          <p class="lead">一部住戸には、室外の給気口・排気口に、外部からの騒音に対する防音性能を備えた、防音フードを採用しました。<br><span>※詳細は係員にお尋ねください。</span></p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit09.png" alt="" class="">
+          </div>
+          <p class="lead">スプーンの落下などの際に発生する軽量床衝撃音への対策として⊿LL（Ⅰ）-4等級相当（メーカー表示）の遮音性能を備えたフローリングを採用し、生活音の伝わりに配慮しました。<br><span>※遮音性能値（⊿L等級）は、JISで定められた測定方法で測定した値であり、実際の住戸内での遮音性を示すものではありません。</span></p>
+          <p class="box">同レベルの音であっても生活する時間帯などにより、音の感じ方には個人差があります。本建物では遮音性に配慮しておりますが、居住者様がお互いに音に対するマナーを守り、快適なマンションライフをお過ごしいただくことが大切となります。</p>
+        </li>
+      </ul>
+      <ul class="lists list02 clearfix">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit10.png" alt="" class="">
+          </div>
+          <p class="lead">住戸間の戸境壁は、鉄筋コンクリート造とし、厚さ約200㎜を基本としています。これは、JISが定める遮音等級Rr-50以上を確保した仕様で、隣接住戸との遮音性に配慮しています。</p>
+          <div class="image img08">
+            <img src="./images/structure01/img08.png" alt="" class="">
+          </div>
+          <p class="caps">「Rr」とは、JISに規定する音響透過損失等級（空気伝搬音の透過のしにくさ）を表す指標で、例えば品確法の性能表示制度では「Rr-50等級以上」の場合には、4等級中上から2番目の等級3（「優れた空気伝搬音の遮断性能が確保されている程度」）に位置付けられています。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit11.png" alt="" class="">
+          </div>
+          <p class="lead">重量床衝撃音対策として、住戸間のコンクリートスラブ厚は約200㎜を確保しています。</p>
+          <div class="image img09">
+            <img src="./images/structure01/img09.png" alt="" class="">
+          </div>
+          <p class="box">同レベルの音であっても生活する時間帯などにより、音の感じ方には個人差があります。本建物では遮音性に配慮しておりますが、居住者様がお互いに音に対するマナーを守り、快適なマンションライフをお過ごしいただくことが大切となります。</p>
+        </li>
+      </ul>
+      <ul class="lists list03 clearfix">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit12.png" alt="" class="">
+          </div>
+          <p class="lead">住戸内の間仕切り壁は厚さ約9.5㎜のプラスターボード貼としていますが、居室（リビング・ダイニング・洋室）が直接バスルーム・パウダールーム・トイレ・キッチンと接する場合は、ボード下地材間にグラスウールを充填し、遮音性に配慮しています。<br><span>※一部除く。詳細は係員にお尋ねください。</span></p>
+          <div class="image img10">
+            <img src="./images/structure01/img10.png" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure01/tit13.png" alt="" class="">
+          </div>
+          <p class="lead">共用廊下側の窓に、羽板が可動式で角度調節が可能なブラインドタイプのルーバー面格子を採用しました。お好みにより光や風を調節しながら効果的に外からの視線を遮ることができ、プライバシー性と防犯性を高めます。</p>
+          <div class="image img11">
+            <img src="./images/structure01/img11.png" alt="" class="">
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section><!-- sec02 -->
+
+
+
+  <p class="page-annotation js-scroll wrap">※掲載の参考写真１は本物件と同等仕様のコンセプトルーム（総合マンションギャラリー秋葉原館）を撮影（2022年8月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれません。 ※掲載の参考写真2はメーカーカタログ等の参考写真であり、本物件のものではありません。 ※設備・仕様はタイプにより異なります。</p>
+</main>
+
+<div class="structure-link structure-link--3">
+    <a href="structure01.html" class="current"><p>基本構造</p></a>
+    <a href="structure02.html"><p>防災対策</p></a>
+    <a href="structure03.html"><p>セキュリティ</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
