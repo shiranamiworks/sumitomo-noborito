b09be8f4e354b63f62600c8629273fc90a1ceac0
橋本真奈美 <manako.075h@gmail.com>
2024-02-29 17:56:59 +0900

    no message

---
+++ develop/equipment01.php
@@ -0,0 +1,428 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "equipment",
+  "gnav_set" => "equipment",
+  "page_class" => "equipment01",
+  "url" => "equipment01.html",
+  "title" => "キッチン・サニタリー｜",
+  "keywords" => "キッチン・サニタリー,",
+  "description" => "キッチン・サニタリーページ。",
+  "add_stylesheet" => ["css/equipment.css","css/equipment01.css"],
+  "add_script" =>  [],
+    
+  "logo_text" => "キッチン・サニタリー｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス登戸｜登戸 マンション｜トップ</a></li>
+      <li><a href="./equipment01.html">設備・仕様</a></li>
+      <li><p>キッチン・サニタリー</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="equipment-link equipment-link--5">
+    <a href="equipment01.html" class="current"><p>キッチン・サニタリー</p></a>
+    <a href="equipment02.html"><p>健康・エコ・省エネ</p></a>
+    <a href="equipment03.html"><p>収納</p></a>
+    <a href="equipment04.html"><p>マルチメディア</p></a>
+    <a href="equipment05.html"><p>快適提案</p></a>
+  </div>
+
+  <section class="sections sec01 clearfix">
+    <div class="wrap">
+      <h2 class="tit">キッチン</h2>
+      <ul class="lists list01">
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit01.png" alt="" class="">
+          </div>
+          <p class="lead">キッチンのカウンタートップには、美しく、高級感あふれる天然石を採用しました。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img01.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit02.png" alt="" class="">
+          </div>
+          <p class="lead">キッチンはフラットな対面カウンターを採用しました。段差がなく最大約900㎜のワイドな奥行きがあるので、配膳する前のお料理の仮置きも持ち上げることなくスムーズにでき、対面側から一緒にお料理を楽しむこともできます。<br><span>※詳細は図面集をご確認ください。</span></p>
+          <div class="image sp">
+            <img src="./images/equipment01/img02.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit03.png" alt="" class="">
+          </div>
+          <p class="lead">水はねや油汚れが付きやすいコンロ前には、メラミン不燃化粧板を採用しました。目地が少なく、汚れも拭き取りやすくなっています。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img03.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list02 pc">
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img01.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img02.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img03.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list03">
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit04.png" alt="" class="">
+          </div>
+          <p class="lead">世界中で愛される、ドイツ・ショット社製の耐熱セラミックガラストッププレートを採用。熱や衝撃に強く、汚れがつきにくいので、お手入れも簡単です。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img04.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+          <div class="image img-sub sp">
+            <img src="./images/equipment01/img04-02.jpg" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit05.png" alt="" class="">
+          </div>
+          <p class="lead">レバー操作ひとつで水量、温度調節可能なシングルレバー水栓を設置。シャワーヘッドを引き出せるので、シンクの掃除などにも便利です。また、浄水カートリッジを内蔵しております。<br><span>※カートリッジ交換費用は別途お客様のご負担となります。</span></p>
+          <div class="image sp">
+            <img src="./images/equipment01/img05.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit06.png" alt="" class="">
+          </div>
+          <p class="lead">手間をかけず、効率よく食器類が洗浄でき、節水効果にも優れた食器洗い乾燥機をご用意しました。スライド式なので、楽な姿勢で食器の出し入れができます。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img06.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list04 pc">
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img04.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+          <div class="image img-sub">
+            <img src="./images/equipment01/img04-02.jpg" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img05.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img06.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list05">
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit07.png" alt="" class="">
+          </div>
+          <p class="lead">システムキッチンの足元近くまでスライド式の収納を設置し、収納量を確保しました。</p>
+          <div class="image">
+            <img src="./images/equipment01/img07.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit08.png" alt="" class="">
+          </div>
+          <p class="lead">システムキッチンには物が取り出しやすく、開閉もスムーズな、スライド式収納を採用しました。</p>
+          <div class="image">
+            <img src="./images/equipment01/img08.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit09.png" alt="" class="">
+          </div>
+          <p class="lead">キッチン空間のインテリアを美しく演出する、シャープなスクエアデザインのレンジフードを採用しました。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit10.png" alt="" class="">
+          </div>
+          <p class="lead">シンク底面に制振素材を貼ることで、シャワー水や食器がシンクに当たるときの耳障りな音を低減し、ご家族の団らんでの会話などに配慮しました。</p>
+        </li>
+      </ul>
+    </div>
+  </section><!-- sec01 -->
+
+
+
+  <section class="sections sec02 clearfix">
+    <div class="wrap">
+      <h2 class="tit">サニタリー</h2>
+      <ul class="lists list01 clearfix">
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit11.png" alt="" class="">
+          </div>
+          <p class="lead">お子様の目線に合わせた三面鏡下鏡を備えた三面鏡洗面化粧台を採用しました。三面鏡の裏側にはコンセント付きの収納棚を確保。スキンケア用品やヘアケア用品、洗面台まわりの電気製品などを使いやすくすっきり整理できます。また収納棚は取り外して掃除ができるので清潔に保てます。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img09.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit12.png" alt="" class="">
+          </div>
+          <p class="lead">カウンターとボウルはお手入れのしやすい継ぎ目のない一体成形で、美しい光沢の有機ガラス系新素材仕様。直線的なスクエアフォルムのボウルが、都会的な空間を演出します。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img10.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit13.png" alt="" class="">
+          </div>
+          <p class="lead">洗面化粧台には、ヘッドが引き出せ、洗面ボウルを洗うときなどに便利なシングルレバー水栓を採用しました。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img11.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list02 pc clearfix">
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img09.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img10.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img11.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list03 clearfix">
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit14.png" alt="" class="">
+          </div>
+          <p class="lead">洗面化粧台のキャビネットの下部を有効利用し、体重計の収納に便利なヘルスメータースペースを設けています。<br><span>※ヘルスメーターのサイズによっては収納できないものもあります。</span></p>
+          <div class="image sp">
+            <img src="./images/equipment01/img12.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit15.png" alt="" class="">
+          </div>
+          <p class="lead">パウダールームには、タオルや肌着類などの収納に便利なリネン庫をご用意。脱衣時や入浴後など、必要なものをすぐに取り出すことができます。</p>
+          <div class="image img13 sp">
+            <img src="./images/equipment01/img13.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit16.png" alt="" class="">
+          </div>
+          <p class="lead">約1.4ｍ×約1.8ｍの大型ユニットバスを採用しました。人にやさしい低床タイプで、快適なバスタイムがお楽しみいただけます。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img14.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list04 pc clearfix">
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img12.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image img13">
+            <img src="./images/equipment01/img13.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img14.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list05 clearfix">
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit17.png" alt="" class="">
+          </div>
+          <p class="lead">乾燥機能で雨の日でも洗濯物を乾かすことができ、換気によりカビの発生も抑制できる東京ガス のＴＥＳ浴室暖房乾燥機を採用しました。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img15.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit18.png" alt="" class="">
+          </div>
+          <p class="lead">指でワンプッシュするだけで簡単に排水することができるプッシュ式の排水栓を採用しました。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img16.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit19.png" alt="" class="">
+          </div>
+          <p class="lead">音楽プレイヤーをキッチンリモコンに接続すると、バスルーム内で音楽や音声プログラムを楽しむことができます。好きな曲がBGMとしてバスルームリモコンから流れ、一日の疲れを癒してくれます。</p>
+          <div class="image img17 sp">
+            <img src="./images/equipment01/img17.png" alt="" class="">
+            <p class="annotation gothic">（参考写真２）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list06 pc clearfix">
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img15.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img16.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image img17">
+            <img src="./images/equipment01/img17.png" alt="" class="">
+            <p class="annotation gothic">（参考写真２）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list07 clearfix">
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit20.png" alt="" class="">
+          </div>
+          <p class="lead">浴槽へのお湯張り、追い焚き、保温までスイッチひとつで自動操作できるオートバスシステムを採用しました。また、キッチンとバスルームに設置したコントローラーで相互通話も可能です。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img18.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit21.png" alt="" class="">
+          </div>
+          <p class="lead">専用のフロフタと浴槽保温材で温まったお湯を冷めにくくしました。湯温が長持ちするので追い焚きや足し湯が節約でき経済的です。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img19.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit22.png" alt="" class="">
+          </div>
+          <p class="lead">トイレには、コンパクトな手洗いカウンターを設けています。すっきりとしたデザインで見た目にも広さとゆとりを感じることができます。</p>
+          <div class="image sp">
+            <img src="./images/equipment01/img20.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list08 pc clearfix">
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img18.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img19.png" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment01/img20.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list09 clearfix">
+      <li>
+          <div class="tit-img">
+            <img src="./images/equipment01/tit23.png" alt="" class="">
+          </div>
+          <p class="lead">コンパクトなフォルムを実現したローシルエットタンクトイレを採用。温水シャワーが快適に使え、着座センサーによりノンタッチで便器の自動洗浄も行なう先進の機能付きです。また、洗浄使用水量が少ないタイプで、節水はCO2排出の削減にもなり、経済面でも環境面でも優しい仕様です。</p>
+        </li>
+      </ul>
+    </div>
+  </section><!-- sec02 -->
+
+
+
+  <p class="page-annotation js-scroll wrap">※掲載の参考写真１は本物件と同等仕様のコンセプトルーム（総合マンションギャラリー秋葉原館）を撮影（2022年8月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれません。 ※掲載の参考写真2はメーカーカタログ等の参考写真であり、本物件のものではありません。 ※設備・仕様はタイプにより異なります。</p>
+</main>
+
+  <div class="equipment-link equipment-link--5">
+    <a href="equipment01.html" class="current"><p>キッチン・サニタリー</p></a>
+    <a href="equipment02.html"><p>健康・エコ・省エネ</p></a>
+    <a href="equipment03.html"><p>収納</p></a>
+    <a href="equipment04.html"><p>マルチメディア</p></a>
+    <a href="equipment05.html"><p>快適提案</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
