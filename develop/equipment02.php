ac0dfd8ba473c921098730da4721560e3ad80b88
福井彩佳 <ayaka.fsp@gmail.com>
2024-03-04 16:15:41 +0900

    no message

---
+++ develop/equipment02.php
@@ -0,0 +1,273 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "equipment",
+  "gnav_set" => "equipment",
+  "page_class" => "equipment02",
+  "url" => "equipment02.html",
+  "title" => "健康・エコ・省エネ｜",
+  "keywords" => "健康・エコ・省エネ,",
+  "description" => "健康・エコ・省エネページ。",
+  "add_stylesheet" => ["css/equipment.css","css/equipment02.css"],
+  "add_script" =>  [],
+    
+  "logo_text" => "健康・エコ・省エネ｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス登戸｜登戸 マンション｜トップ</a></li>
+      <li><a href="./equipment01.html">設備・仕様</a></li>
+      <li><p>健康・エコ・省エネ</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="equipment-link equipment-link--5">
+    <a href="equipment01.html"><p>キッチン・サニタリー</p></a>
+    <a href="equipment02.html" class="current"><p>健康・エコ・省エネ</p></a>
+    <a href="equipment03.html"><p>収納</p></a>
+    <a href="equipment04.html"><p>マルチメディア</p></a>
+    <a href="equipment05.html"><p>快適提案</p></a>
+  </div>
+
+  <section class="sections sec01 clearfix">
+    <div class="wrap">
+      <h2 class="tit">健康・エコ・省エネ</h2>
+      <ul class="lists list01">
+        <li class="double">
+          <div class="tit-img">
+            <img src="./images/equipment02/tit01.png" alt="" class="pc">
+            <img src="./images/equipment02/tit01-sp.png" alt="" class="sp">
+          </div>
+          <p class="lead">東京ガスの高効率ガス給湯器「エコジョーズ」を採用。キッチン、バスルーム、パウダールームへのスムーズな給湯はもちろん、床暖房や浴室暖房乾燥機までをトータルにサポートします。また、この熱源システムは、従来無駄に捨てられていた排気熱、潜熱を効率よく回収してお湯を沸かす省エネ仕様となっており、従来の給湯器に比べて環境に優しく、年間ランニングコストの面においても優れた経済性を発揮します。</p>
+          <p class="text01 tb">※1 FT4215ARSAW6Qの場合（スタンダードの一例）※2 FT4220・FT4222シリーズの場合（エコジョーズの一例）※1※2はJIS S 2109で定める測定方法により算定 ただし※1～2ともご使用状況により熱効率は異なります。※3 試算条件：ガス使用量は季節に応じて変化するため、家庭用の平均的な月別使用量比率に従って年間ガス使用量を各月に割り付けたうえで試算。居間の暖房はガス温水床暖房と電気エアコンを併用、居間以外の暖房及び冷房は電気エアコン使用、浴室暖房乾燥機使用（衣類乾燥週2回/年、浴室暖房週7回/冬期）。機器の運転状況等により削減量は異なります。機器の運転状況により熱源機の熱効率は変動するため、熱効率を一定にしたうえで試算。CO2排出係数はガス2.29kg-CO2/?（東京ガスデータ）</p>
+          <div class="image tb">
+            <img src="./images/equipment02/img01.png" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit02.png" alt="" class="">
+          </div>
+          <p class="lead">キッチンとバスルームの給湯リモコンには「エコ運転機能」を搭載。画面メニューから「エコ」をタッチするだけで給湯量が自動調整でき、省エネ・節約に効果的です。また、TES熱源機（エコジョーズ）で使用した一日のガス・お湯の量、使用料金の目安、CO2の排出量を表示する「エネルック機能」も付いています。※「エコ運転機能」を利用して2ヶ所以上でお湯を使うと湯細りする可能性があります。</p>
+          <div class="image tb">
+            <img src="./images/equipment02/img02.png" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list02 pc">
+        <li class="double">
+          <div class="image">
+            <img src="./images/equipment02/img01.png" alt="" class="">
+            <p class="annotation gothic">（参考写真2）</p>
+          </div>
+          <p class="text01">※1 FT4215ARSAW6Qの場合（スタンダードの一例）※2 FT4220・FT4222シリーズの場合（エコジョーズの一例）※1※2はJIS S 2109で定める測定方法により算定 ただし※1～2ともご使用状況により熱効率は異なります。※3 試算条件：ガス使用量は季節に応じて変化するため、家庭用の平均的な月別使用量比率に従って年間ガス使用量を各月に割り付けたうえで試算。居間の暖房はガス温水床暖房と電気エアコンを併用、居間以外の暖房及び冷房は電気エアコン使用、浴室暖房乾燥機使用（衣類乾燥週2回/年、浴室暖房週7回/冬期）。機器の運転状況等により削減量は異なります。機器の運転状況により熱源機の熱効率は変動するため、熱効率を一定にしたうえで試算。CO2排出係数はガス2.29kg-CO2/㎡（東京ガスデータ）</p>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment02/img02.png" alt="" class="">
+            <p class="annotation gothic">（参考写真2）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list03">
+        <li  class="double">
+          <div class="tit-img">
+            <img src="./images/equipment02/tit03.png" alt=""  class="pc">
+            <img src="./images/equipment02/tit03-sp.png" alt="" class="sp">
+          </div>
+          <p class="lead">生ゴミを、キッチンですばやく粉砕処理できるシステムです。各住戸のディスポーザーで破砕された生ゴミは専用の排水処理装置で浄化してから、排水するので、環境への負荷も軽減することができます。<br><span>※一部処理できないものもあります。</span></p>
+          <div class="image tb">
+            <img src="./images/equipment02/img03.jpg" alt="" class="">
+          </div>
+          <div class="image img-sub tb">
+            <img src="./images/equipment02/img03-02.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真2）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit04.png" alt="" class="">
+          </div>
+          <p class="lead">自然エネルギーを有効に活用した太陽光発電システムを屋上に設置。マンションの共用部で使用する電力の一部を賄う事ができます。</p>
+          <div class="image tb">
+            <img src="./images/equipment02/img04.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真2）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list04 pc">
+        <li class="double">
+          <div class="image">
+            <img src="./images/equipment02/img03.jpg" alt="" class="">
+          </div>
+          <div class="image img-sub">
+            <img src="./images/equipment02/img03-02.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真2）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment02/img04.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list05">
+        <li class="double">
+          <div class="tit-img">
+            <img src="./images/equipment02/tit05.png" alt=""  class="pc">
+            <img src="./images/equipment02/tit05-sp.png" alt="" class="sp">
+          </div>
+          <p class="lead">システムキッチンの足元近くまでスライド式の収納を設置し、収納量を確保しました。</p>
+          <div class="image">
+            <img src="./images/equipment02/img05.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真2）</p>
+          </div>
+          <div class="image img-sub">
+            <img src="./images/equipment02/img05-02.jpg" alt="" class="">
+            <p class="annotation gothic">※参考図（天井高は実際とは異なります。）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit06.png" alt="" class="">
+          </div>
+          <p class="lead">窓を閉めたままでも常に換気ができる、24時間常時小風量換気システムを採用しています。各室の自然給気口から外気を取り入れ、ドア下を経由して、バスルーム、パウダールーム、トイレの換気口から室内の空気を排出します。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit07.png" alt="" class="">
+          </div>
+          <p class="lead">窓を閉めたままでも常に換気ができる、24時間常時小風量換気システムを採用しています。各室の自然給気口から外気を取り入れ、ドア下を経由して、バスルーム、パウダールーム、トイレの換気口から室内の空気を排出します。</p>
+        </li>
+      </ul>
+      <ul class="lists list06 clearfix">
+        <li class="full">
+          <div class="tit-img">
+            <img src="./images/equipment02/tit08.png" alt=""  class="pc">
+            <img src="./images/equipment02/tit08-sp.png" alt="" class="sp">
+          </div>
+          <p class="lead">一般のドライサウナと比べ、低温・高湿度で息苦しくなくリラックスして楽しめます。気分をリフレッシュしたいとき、お肌をやさしくいたわりたいとき、ミストサウナ入浴で素敵なバスタイムを。さらに、衣類のリフレッシュや浴室のカビ抑制により、家事の負担を軽減します。</p>
+          <div class="image pc">
+            <img src="./images/equipment02/img08.jpg" alt="" class="">
+          </div>
+          <div class="image tb">
+            <img src="./images/equipment02/img08-01.jpg" alt="" class="">
+            <img src="./images/equipment02/img08-02.jpg" alt="" class="">
+            <img src="./images/equipment02/img08-03.jpg" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit09.png" alt="" class="">
+          </div>
+          <p class="lead">住戸の一部の開口部には省エネ効果に優れたLow-Eガラスを採用。ガラスの表面にコーティングされた日射熱の反射性を高める特殊金属膜（Low-E膜）と断熱性を高める空気層により、冷暖房両方の負荷を軽減します。<span>※詳細は係員にお尋ねください。</span></p>
+          <div class="image tb">
+            <img src="./images/equipment02/img09.jpg" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit10.png" alt="" class="">
+          </div>
+          <p class="lead">一部住戸の開口部には、断熱性能に優れた真空ガラスを使用しています。真空ガラスは2枚のガラスの間に真空層をつくることで、熱の「伝導」と「対流」を防ぎ、「放射」は特殊金属膜で抑えています。そのため冬は暖房負荷を軽減し、夏は日射熱を反射して「冬は暖かく、夏は涼しい」環境をつくりだします。</p>
+          <div class="image tb">
+            <img src="./images/equipment02/img10.jpg" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit11.png" alt="" class="">
+          </div>
+          <p class="lead">外壁のコンクリート厚は、約150㎜?約250㎜を確保し、室内側には断熱材を吹き付けて、省エネにも配慮しています。</p>
+          <div class="image tb">
+            <img src="./images/equipment02/img11.jpg" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list07 pc clearfix">
+        <li>
+          <div class="image">
+            <img src="./images/equipment02/img09.jpg" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment02/img10.jpg" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/equipment02/img11.jpg" alt="" class="">
+            <p class="annotation gothic">（概念図）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list08">
+        <li class="double">
+          <div class="tit-img">
+            <img src="./images/equipment02/tit12.png" alt=""  class="pc">
+            <img src="./images/equipment02/tit12-sp.png" alt="" class="sp">
+          </div>
+          <p class="lead">システムキッチンの足元近くまでスライド式の収納を設置し、収納量を確保しました。</p>
+          <div class="image">
+            <img src="./images/equipment02/img12.jpg" alt="" class="">
+          </div>
+          <div class="image img-sub">
+            <img src="./images/equipment02/img12-02.jpg" alt="" class="">
+            <p class="annotation"><span class="gothic">（概念図）</span><br>※【条件】給水圧：0.1MPa 給湯圧：0.1MPa レバー中央（水）位置での値</p>
+            
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit13.png" alt="" class="">
+          </div>
+          <p class="lead">建材や内装材などの素材（建築基準法上の特定建材）には、ＪＩＳやＪＡＳによって定められたホルムアルデヒドの発散量が少ないものを採用。健康的な住まいを追求しています。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit14.png" alt="" class="">
+          </div>
+          <p class="lead">天井・床および外気に面する柱・梁・壁の室内側に約12㎜～約50㎜厚、最下階住戸の床下に約30㎜～約50㎜厚の断熱材を施工。また、屋上には約50㎜厚の断熱材を施しています。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/equipment02/tit15.png" alt="" class="">
+          </div>
+          <p class="lead">住戸内のダウンライトにはLED照明を採用。従来の白熱灯に比べ寿命が長持ちし、消費電力量とCO2排出量が削減されます。</p>
+        </li>
+      </ul>
+    </div>
+  </section><!-- sec01 -->
+
+
+
+  <p class="page-annotation js-scroll wrap">※掲載の参考写真1は本物件と同等仕様のコンセプトルーム（総合マンションギャラリー秋葉原館）を撮影（2022年8月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれません。 ※掲載の参考写真2はメーカーカタログ等の参考写真であり、本物件のものではありません。 ※設備・仕様はタイプにより異なります。</p>
+</main>
+
+  <div class="equipment-link equipment-link--5">
+    <a href="equipment01.html" class="current"><p>キッチン・サニタリー</p></a>
+    <a href="equipment02.html"><p>健康・エコ・省エネ</p></a>
+    <a href="equipment03.html"><p>収納</p></a>
+    <a href="equipment04.html"><p>マルチメディア</p></a>
+    <a href="equipment05.html"><p>快適提案</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
