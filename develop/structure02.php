2dcb56e1e6245810c27269cb2e6552a4ec64968c
橋本真奈美 <manako.075h@gmail.com>
2024-03-04 17:02:19 +0900

    no message

---
+++ develop/structure02.php
@@ -0,0 +1,195 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "structure",
+  "gnav_set" => "structure",
+  "page_class" => "structure02",
+  "url" => "structure02.html",
+  "title" => "防災対策｜",
+  "keywords" => "防災対策,",
+  "description" => "防災対策ページ。",
+  "add_stylesheet" => ["css/structure.css","css/structure02.css"],
+  "add_script" =>  [],
+    
+  "logo_text" => "防災対策｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス登戸｜登戸 マンション｜トップ</a></li>
+      <li><a href="./structure01.html">構造・防災</a></li>
+      <li><p>防災対策</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="structure-link structure-link--3">
+    <a href="structure01.html"><p>基本構造</p></a>
+    <a href="structure02.html" class="current"><p>防災対策</p></a>
+    <a href="structure03.html"><p>セキュリティ</p></a>
+  </div>
+
+  <section class="sections sec01 clearfix">
+    <div class="wrap">
+      <h2 class="tit">防災対策</h2>
+      <ul class="lists list01">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit01.png" alt="" class="">
+          </div>
+          <p class="lead">エレベーター運転中に、地震管制装置が一定値を超えた地震の初期微動（Ｐ波）・主要動（S波）を感知すると、最寄階に速やかに停止します。また、停電した際には一旦停止後、停電時自動着床装置により、最寄階に自動停止し、さらに、天井の停電灯が点灯してエレベーター内を照らす他、インターホンが使用できるので、外部との連絡も可能です。<br><span>※エレベーター運転中に急速な大きい地震により、主要動（S波）［高］を感知した場合は、その瞬間に直ちに休止し、最寄階への移動や扉が開かない場合がございます。</span></p>
+          <div class="image img01">
+            <img src="./images/structure02/img01.png" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit02.png" alt="" class="">
+          </div>
+          <p class="lead">地震などの非常時に備え、防災備品をご用意しました。初期共助に使用する小型拡声器、救助用工具セット、救助用ロープ、スコップ、防塵マスクなどを備えています。また、災害時のライフサポート備品として救急箱、携帯電話充電・懐中電灯機能付き手回し充電ラジオ、カセットガス発電機、非常用ロウソク、飲料水用の折りたたみ式給水ポリタンク、災害保存用飲料水、災害用簡易トイレなども備えています。</p>
+          <div class="image">
+            <img src="./images/structure02/img02.png" alt="" class="">
+          </div>
+        </li>
+      </ul>
+
+      <ul class="lists list02">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit03.png" alt="" class="">
+          </div>
+          <p class="lead">1階共用部の共用トイレ、共用水栓（屋外散水栓、1階メンテナンス用水栓、ペット足洗い場、ゴミ置場水栓、管理事務室内ミニキッチン、清掃員控室内ミニキッチン）へは水道本管から給水ポンプを経由せずに給水しているので、万一の停電で給水ポンプが停止しても、水を使うことができます。<br><span>※被災の状況により水が使えない場合があります。</span></p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit04.png" alt="" class="">
+          </div>
+          <p class="lead">地震などの非常時に役立つグッズをまとめた防災リュックをお引渡し時に各住戸へお渡しします。<br><span>※飲料水や食品等は含まれておりません。詳細は係員にお尋ねください。</span></p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit05.png" alt="" class="">
+          </div>
+          <p class="lead">災害時などに安全に避難できるようエントランス、エレベーターホール、C棟北側出入口には強化ガラスを採用（一部除く）しました。強化ガラスは衝撃に強く割れにくい上に、万一割れた場合でも破片の角が鋭利にならないという特徴があります。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit06.png" alt="" class="">
+          </div>
+          <p class="lead">火災が発生した時、移動して消火活動ができる消火器が共用部の各所に設置されています。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit07.png" alt="" class="">
+          </div>
+          <p class="lead">火災が発生した時、初期消火を行うことができる屋内消火栓が共用部の各所に設置されています。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit08.png" alt="" class="">
+          </div>
+          <p class="lead">災害時に敷地内の特定のマンホールのフタを外して簡易トイレ（非常用マンホールトイレ）を設置するだけですぐに利用できます。</p>
+        </li>
+      </ul>
+
+      <ul class="lists list03">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit09.png" alt="" class="">
+          </div>
+          <p class="lead">万一火災などが発生した場合は、火災感知器が作動。住戸内インターホン親機と玄関前インターホン（子機）より警報が鳴り、室内外に異常をお知らせするとともに、管理事務室を経由して信号が警備会社、住友不動産建物サービス設備管理センターへ自動通報されます。</p>
+          <div class="image sp">
+            <img src="./images/structure02/img03.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真２）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit10.png" alt="" class="">
+          </div>
+          <p class="lead">万一ガス漏れが発生した場合は、ガス漏れ検知器が作動。当該住戸内のインターホン親機と玄関前インターホン（子機）より警報音が鳴り、室内外に異常をお知らせするとともに、管理事務室を経由して信号が警備会社および住友不動産建物サービス設備管理センターへ自動通報されます。</p>
+          <div class="image sp">
+            <img src="./images/structure02/img04.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真２）</p>
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit11.png" alt="" class="">
+          </div>
+          <p class="lead">洗面化粧台の三面鏡収納の扉には、地震の揺れによって扉が開いて、収納物が落下しないように、耐震ラッチを設置。住まう方の安全に配慮しています。</p>
+          <div class="image sp">
+            <img src="./images/structure02/img05.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+      <ul class="lists list04 pc">
+        <li>
+          <div class="image">
+            <img src="./images/structure02/img03.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真２）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/structure02/img04.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真２）</p>
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/structure02/img05.jpg" alt="" class="">
+            <p class="annotation gothic">（参考写真１）</p>
+          </div>
+        </li>
+      </ul>
+
+      <ul class="lists list05">
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit12.png" alt="" class="">
+          </div>
+          <p class="lead">災害発生時、バルコニーを通って下階に降りられるよう各階バルコニーの必要箇所に避難ハッチを設けました。避難ハッチのない住戸は、バルコニーの隔板を破って同一階で移動することにより、避難ハッチを利用することができます。</p>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit13.png" alt="" class="">
+          </div>
+          <p class="lead">万一の地震時に、玄関扉の枠が歪んでも、枠と扉の隙間を大きく設けることにより、扉の開放を容易にできるように配慮した対震ドア枠を採用しました。</p>
+          <div class="image img06">
+            <img src="./images/structure02/img06.png" alt="" class="">
+          </div>
+        </li>
+        <li>
+          <div class="tit-img">
+            <img src="./images/structure02/tit14.png" alt="" class="">
+          </div>
+          <p class="lead">万一の地震時による家具転倒予防として、キッチンの壁面の一部に家具固定用下地補強を施しました。壁面の一部分にある下地補強に家具を固定することで家具が倒れにくくなり、住まう方の安全に配慮された仕様です。<br><span>※取付金具は別途お客様のご負担となります。</span></p>
+          <div class="image">
+            <img src="./images/structure02/img07.png" alt="" class="">
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section><!-- sec01 -->
+
+
+
+  <p class="page-annotation js-scroll wrap">※掲載の参考写真１は本物件と同等仕様のコンセプトルーム（総合マンションギャラリー秋葉原館）を撮影（2022年8月）したもので、本物件のものではありません。家具・調度品等オプション仕様は販売価格に含まれません。 ※掲載の参考写真2はメーカーカタログ等の参考写真であり、本物件のものではありません。 ※設備・仕様はタイプにより異なります。</p>
+</main>
+
+<div class="structure-link structure-link--3">
+    <a href="structure01.html"><p>基本構造</p></a>
+    <a href="structure02.html" class="current"><p>防災対策</p></a>
+    <a href="structure03.html"><p>セキュリティ</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
