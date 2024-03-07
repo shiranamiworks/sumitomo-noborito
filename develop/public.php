00f840a2f31a737b2732c444c4287e72c3a2c43e
福井彩佳 <ayaka.fsp@gmail.com>
2024-03-07 09:49:27 +0900

    no message

--- develop/public.php
+++ develop/public.php
@@ -29,7 +29,7 @@
 
   <header class="main-visual">
     <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
-   <div class="image"><img src="./images/public/mv01.jpg" alt="" class="pc"><img src="./images/public/mv01-sp.jpg" alt="" class="sp"><p class="annotation white">エントランス完成予想図</p></div>
+   <div class="image"><img src="./images/public/mv01.jpg" alt="" class="pc"><img src="./images/public/mv01-sp.jpg" alt="" class="tb"><p class="annotation white">エントランス完成予想図</p></div>
   </header><!-- main-visual -->
 
   <section class="intro sections">
